0437a5739a293832f7d6c7e19dc41c8fe816aa22
Aleksander Dreszler <olek.d93@gmail.com>
2023-06-16 18:50:20 +0200

    Templates created. Table template TODO

--- src/main/java/pl/adreszler/datagenerator/DataGeneratorApplication.java
+++ src/main/java/pl/adreszler/datagenerator/DataGeneratorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataGeneratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataGeneratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataGeneratorApplication.class, args);
+    }
 
 }
